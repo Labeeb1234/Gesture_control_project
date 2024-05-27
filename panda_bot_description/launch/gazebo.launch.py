81d5959f623818d6cd4f9709a33b4ec2efdf5328
Labeeb Nassar <132001019@smail.iitpkd.ac.in>
2024-05-27 13:32:47 +0530

    welder_bot_description

---
+++ panda_bot_description/launch/gazebo.launch.py
@@ -0,0 +1,95 @@
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess, TimerAction
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('panda_bot_description')
+    # moveit_config_pkg_dir = get_package_share_directory('panda_moveit_config')
+
+    xacro_file = os.path.join(share_dir, 'urdf', 'welder.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_urdf}
+        ]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'pause': 'true'
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+
+    urdf_spawn_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'panda',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = Node(
+        package='controller_manager',
+        executable='spawner',
+        name='joint_trajectory_controller',
+        arguments=['joint_trajectory_controller', '--controller-manager', '/controller_manager']
+    )
+
+    load_joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        name='joint_state_broadcaster',
+        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager']
+    )
+
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        gazebo_server,
+        gazebo_client,
+        urdf_spawn_node,
+        TimerAction(period=1.0,
+            actions=[load_joint_trajectory_controller,
+            load_joint_state_broadcaster])
+    ])
